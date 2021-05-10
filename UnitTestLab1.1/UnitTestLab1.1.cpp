740554ede9a7a7a2ba7ed3aec1d6c9ccad7d1050
Denys Tsybko <72654542+edexmusic@users.noreply.github.com>
2021-05-10 14:26:13 +0300

    Add files via upload

---
+++ UnitTestLab1.1/UnitTestLab1.1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab1.1/intRange.h"
+#include "../lab1.1/intRAnge.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab11
+{
+	TEST_CLASS(UnitTestLab11)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			intRAnge a;
+			a.Init(1, 4);
+			Assert::IsTrue(a.rangeCheck(3));
+		}
+	};
+}
